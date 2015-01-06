73878b102f1330708e63cb520b54b3e6936a4a2c
Ramiro Berrelleza <rberrelleza@atlassian.com>
2015-01-06 00:13:42 -0800

    Added index page

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-chmod -R 744 /botserver/static
+mkdir -p /web
+chmod 755 web
+cp static/* /web
+
 rm /etc/nginx/sites-enabled/*
-cp -r sites-enabled/ /etc/nginx/sites-enabled
+cp /botserver/sites-enabled/* /etc/nginx/sites-enabled
 service nginx restart
